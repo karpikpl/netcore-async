5de6d440f5753be2450f25d52d1f8c7bbf3a8c63
Piotr Karpala <karpik.pl@gmail.com>
2018-02-21 19:23:53 -0500

    code comments

--- InParallel.cs
+++ InParallel.cs
@@ -14,12 +14,15 @@ namespace dotnetasync
 			var bTask =  Delayer.Get(1000);
 			var cTask =  Delayer.Get(5);
 
+			// this is really optional here - it doesn't change the result since we're still read value from each of the tasks
+			await Task.WhenAll(aTask, bTask, cTask);
+
 			var a = await aTask;
 			var b = await bTask;
 			var c = await cTask;
 			sw.Stop();
 
-			Console.WriteLine($"Async completed - got {a} {b} {c} in {sw.ElapsedMilliseconds}\n");
+			Console.WriteLine($"Async completed - got {a} {b} {c} in {sw.ElapsedMilliseconds}\n\t");
 		}
 	}
 }
